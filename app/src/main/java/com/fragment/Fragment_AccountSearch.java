2460653b04b3eed8aa6f14c432b5fd0729dcdd3c
tutulyy <lyyoicq@163.com>
2015-07-27 16:10:51 +0800

    2015-7-27 16:10

--- app/src/main/java/com/fragment/Fragment_AccountSearch.java
+++ app/src/main/java/com/fragment/Fragment_AccountSearch.java
@@ -4,17 +4,14 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.myapplication.Activity_AccountList;
 import com.myapplication.R;
 
 import java.util.ArrayList;
@@ -25,17 +22,21 @@ import java.util.Map;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Fragment_AccountList.OnFragmentInteractionListener} interface
+ * {@link Fragment_AccountSearch.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Fragment_AccountList#newInstance} factory method to
+ * Use the {@link Fragment_AccountSearch#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment_AccountList extends Fragment {
+public class Fragment_AccountSearch extends Fragment {
 
-    private List<Map<String,Object>> dataList = new ArrayList<Map<String, Object>>();
-    private SimpleAdapter adapter ;
-    private ListView listView ;
-    TextView textView ;
+    Button buttonGo;
+    Button buttonCancle;
+    EditText editTextName;
+    EditText editTextMoney;
+    CheckBox checkBox_HuoQi;
+    CheckBox checkBox_DingQi;
+    CheckBox checkBox_QingHu;
+    Map<String, String> params = new HashMap<String, String>();
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -46,16 +47,8 @@ public class Fragment_AccountList extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_AccountList.
-     */
-    public static Fragment_AccountList newInstance(String param1, String param2) {
-        Fragment_AccountList fragment = new Fragment_AccountList();
+    public static Fragment_AccountSearch newInstance(String param1, String param2) {
+        Fragment_AccountSearch fragment = new Fragment_AccountSearch();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,7 +56,7 @@ public class Fragment_AccountList extends Fragment {
         return fragment;
     }
 
-    public Fragment_AccountList() {
+    public Fragment_AccountSearch() {
         // Required empty public constructor
     }
 
@@ -79,7 +72,7 @@ public class Fragment_AccountList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_account_list, null);
+        View view = inflater.inflate(R.layout.fragment_account_search, null);
         return view;
     }
 
@@ -88,43 +81,50 @@ public class Fragment_AccountList extends Fragment {
         initData();
         initEvent();
         super.onActivityCreated(savedInstanceState);
-    }
-
-
-    /*
-    重新加载查询页面
-     */
-    public void onGetSearchData(Map<String, String> params) {
 
     }
+
     private void initWidget() {
 
-        listView = (ListView) getActivity().findViewById(R.id.listView);
-        textView = (TextView) getActivity().findViewById(R.id.worning);
-        textView.setVisibility(View.GONE);
+        buttonGo = (Button) getActivity().findViewById(R.id.button);
+        buttonCancle = (Button) getActivity().findViewById(R.id.button2);
+        editTextName = (EditText) getActivity().findViewById(R.id.editText);
+        editTextMoney = (EditText) getActivity().findViewById(R.id.editText2);
+        checkBox_HuoQi = (CheckBox) getActivity().findViewById(R.id.checkBox1);
+        ;
+        checkBox_DingQi = (CheckBox) getActivity().findViewById(R.id.checkBox2);
+        ;
+        checkBox_QingHu = (CheckBox) getActivity().findViewById(R.id.checkBox3);
+        ;
+    }
 
+    private void initData() {
     }
-    private  void initData(){
-        for (int i = 0 ; i < 10 ; i ++ ){
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("name","战三");
-            map.put("money","800004.56");
-            map.put("account","54684984896849698498");
-            dataList.add(map);
-        }
-        adapter = new SimpleAdapter(getActivity().getApplicationContext(), dataList, R.layout.listview_accountlist_item,
-                new String[]{"name","money","account"}, new int[]{R.id.listname, R.id.listmoney, R.id.account});
-        listView.setAdapter(adapter);
+
+    private void reSetForm() {
+        editTextName.setText("");
+        editTextMoney.setText("");
+        checkBox_HuoQi.setChecked(false);
+        checkBox_DingQi.setChecked(false);
+        checkBox_QingHu.setChecked(false);
     }
-    private void initEvent(){
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+    private void upParams() {
+        mListener.onFragmentSearchInteraction(params);
+    }
+
+    private void initEvent() {
+        buttonGo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                int selectedPosition = i;
-                Log.i("fragment_log", String.valueOf(selectedPosition));
-                ((Activity_AccountList) getActivity()).switchContent();
-                //setContentView(R.layout.activity_account_info);
-                //isIndexView = false;
+            public void onClick(View view) {
+                upParams();
+            }
+        });
+        buttonCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reSetForm();
+                upParams();
             }
         });
     }
@@ -157,7 +157,7 @@ public class Fragment_AccountList extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        public void onFragmentInteraction(Uri uri);
+        // TODO: Update argument type and name
+        public void onFragmentSearchInteraction(Map<String, String> params);
     }
-
 }
--- app/src/main/java/com/myapplication/Activity_AccountList.java
+++ app/src/main/java/com/myapplication/Activity_AccountList.java
@@ -1,6 +1,5 @@
 package com.myapplication;
 
-import android.app.Fragment;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -11,14 +10,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.fragment.Fragment_AccountInfo;
 import com.fragment.Fragment_AccountList;
+import com.fragment.Fragment_AccountSearch;
+
+import java.util.Map;
 
 
 public class Activity_AccountList extends FragmentActivity
-        implements Fragment_AccountList.OnFragmentInteractionListener,Fragment_AccountInfo.OnFragmentInteractionListener {
+        implements
+        Fragment_AccountList.OnFragmentInteractionListener,
+        Fragment_AccountInfo.OnFragmentInteractionListener,
+        Fragment_AccountSearch.OnFragmentInteractionListener {
 
 
     private boolean isIndexView ;
@@ -27,22 +31,22 @@ public class Activity_AccountList extends FragmentActivity
     FragmentManager fragmentManager ;
     Fragment_AccountList fragment_accountList ;
     Fragment_AccountInfo fragment_accountInfo ;
+    Fragment_AccountSearch fragment_accountSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_account_list);
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_account);
-
         initWidget();
         initData();
         initEvent();
-
     }
 
     private void initWidget() {
         fragment_accountInfo = new Fragment_AccountInfo();
         fragment_accountList = new Fragment_AccountList();
+        fragment_accountSearch = new Fragment_AccountSearch();
         fragmentManager = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.frame, fragment_accountList);
@@ -61,13 +65,22 @@ public class Activity_AccountList extends FragmentActivity
                 onTitleBackButtonClick();
             }
         });
+        imageButton_Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onTitleSearchButtonClick();
+            }
+        });
     }
     public void switchContent() {
         if (isIndexView) {
             imageButton_Search.setVisibility(View.GONE);
             isIndexView = !isIndexView ;
             android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setCustomAnimations(
-                    android.R.anim.fade_in, R.anim.abc_fade_out);
+                    R.anim.my_slide_in_right, R.anim.abc_fade_out);
+            if (fragment_accountSearch.isAdded()) {    // 先判断是否被add过
+                fragmentTransaction.remove(fragment_accountSearch); // 隐藏当前的fragment，显示下一个
+            }
             if (!fragment_accountInfo.isAdded()) {    // 先判断是否被add过
                 fragmentTransaction.hide(fragment_accountList).add(R.id.frame, fragment_accountInfo).commit(); // 隐藏当前的fragment，add下一个到Activity中
             } else {
@@ -78,7 +91,7 @@ public class Activity_AccountList extends FragmentActivity
             imageButton_Search.setVisibility(View.VISIBLE);
             isIndexView = !isIndexView ;
             android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setCustomAnimations(
-                    android.R.anim.fade_in, R.anim.abc_fade_out);
+                    android.R.anim.fade_in, R.anim.my_slide_out_right);
             if (!fragment_accountList.isAdded()) {    // 先判断是否被add过
                 fragmentTransaction.hide(fragment_accountInfo).add(R.id.frame, fragment_accountList).commit(); // 隐藏当前的fragment，add下一个到Activity中
             } else {
@@ -87,12 +100,24 @@ public class Activity_AccountList extends FragmentActivity
         }
     }
     private void onTitleBackButtonClick(){
-        if (isIndexView){
+        if (fragment_accountSearch.isAdded()) {    // 先判断是否被add过
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(fragment_accountSearch).commit(); // 隐藏当前的fragment，显示下一个
+        } else if (isIndexView) {
             Activity_AccountList.this.setResult(RESULT_OK);
             Activity_AccountList.this.finish();
         }
         else{
             switchContent();
+        }
+    }
+
+    private void onTitleSearchButtonClick() {
+        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (!fragment_accountSearch.isAdded()) {    // 先判断是否被add过
+            fragmentTransaction.add(R.id.frame_search, fragment_accountSearch).commit();
+        } else {
+            fragmentTransaction.remove(fragment_accountSearch).commit(); // 隐藏当前的fragment，显示下一个
         }
     }
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -128,5 +153,13 @@ public class Activity_AccountList extends FragmentActivity
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    public void onFragmentSearchInteraction(Map<String, String> params) {
+        onTitleSearchButtonClick();
+        if (fragment_accountList != null) {
+            fragment_accountList.onGetSearchData(params);
+        }
     }
 }
